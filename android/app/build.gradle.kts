f958550833353f8753fb6c5a5c61b0c369c2097c
lessurvinz120204 <lessurvinz@gmail.com>
2026-05-04 15:23:21 +0800

    fixed

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -20,10 +20,6 @@ android {
         targetCompatibility = JavaVersion.VERSION_17
     }
 
-    kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_17.toString()
-    }
-
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.water_watch"
